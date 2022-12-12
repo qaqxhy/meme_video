a32added0660448d810452a5c8240e584709a051
qaqxhy <iam_harry@163.com>
2022-12-11 22:17:20 -0500

    update

--- Plugin.php
+++ Plugin.php
@@ -5,15 +5,18 @@
  * @package meme_video
  * @author 2F1A
  * @version 0.1
- * @link 
+ * @link https://github.com/qaqxhy/meme_video
  */
 
-class meme_video_Plugin implements Typecho_Plugin_Interface{
+if (!defined('__TYPECHO_ROOT_DIR__')) {
+    exit;
+}
+class memevideo_Plugin implements Typecho_Plugin_Interface{
 
     /* 激活插件方法 */
     public static function activate(){
-        Typecho_Plugin::factory('Widget_Archive') -> header = array('meme_video_Plugin', 'header');
-        Typecho_Plugin::factory('Widget_Archive') -> footer = array('meme_video_Plugin', 'footer');
+        Typecho_Plugin::factory('Widget_Archive') -> header = array('memevideo_Plugin', 'header');
+        Typecho_Plugin::factory('Widget_Archive') -> footer = array('memevideo_Plugin', 'footer');
     }
 
     /* 禁用插件方法 */
@@ -21,17 +24,121 @@ class meme_video_Plugin implements Typecho_Plugin_Interface{
 
     /* 插件配置方法 */
     public static function config(Typecho_Widget_Helper_Form $form){
+        $position = new Typecho_Widget_Helper_Form_Element_Radio('position',
+        array(
+          'left' => _t('靠左'),
+          'right' => _t('靠右'),
+        ),
+        'right', _t('自定义位置'), _t('自定义视频所在的位置'));
+        $form -> addInput($position);
 
+        $autoplay = new Typecho_Widget_Helper_Form_Element_Radio('autoplay',
+        array(
+          '0' => _t('关闭'),
+          '1' => _t('开启'),
+        ),
+        '1', _t('自动播放'), _t('开启后将自动播放视频'));
+        $form -> addInput($autoplay);
+
+        $loop = new Typecho_Widget_Helper_Form_Element_Radio('loop',
+        array(
+          '0' => _t('关闭'),
+          '1' => _t('开启'),
+        ),
+        '1', _t('循环播放'), _t('开启后将循环播放视频'));
+        $form -> addInput($loop);
+
+        $muted = new Typecho_Widget_Helper_Form_Element_Radio('muted',
+        array(
+          '0' => _t('关闭'),
+          '1' => _t('开启'),
+        ),
+        '1', _t('静音播放'), _t('开启后将静音播放视频,关闭后将无法使用自动播放'));
+        $form -> addInput($muted);
+
+        $controls = new Typecho_Widget_Helper_Form_Element_Radio('controls',
+        array(
+          '0' => _t('关闭'),
+          '1' => _t('开启'),
+        ),
+        '0', _t('HTML视频控件'), _t('开启后将显示视频控件'));
+        $form -> addInput($controls);
+
+        $src = new Typecho_Widget_Helper_Form_Element_Text('src', NULL, NULL, _t('视频地址'), _t('在这里填入一个视频地址链接'));// !todo 格式支持
+        $form -> addInput($src);
+
+        $custom_width = new Typecho_Widget_Helper_Form_Element_Text('custom_width', NULL, NULL, _t('自定义宽'), _t('在这里填入一个视频宽度,单位px,留空为100'));
+        $form -> addInput($custom_width);
+
+        $custom_height = new Typecho_Widget_Helper_Form_Element_Text('custom_height', NULL, NULL, _t('自定义高'), _t('在这里填入一个视频高度,单位px,留空为100'));
+        $form -> addInput($custom_height);
     }
 
     /* 个人用户的配置方法 */
     public static function personalConfig(Typecho_Widget_Helper_Form $form){}
 
-    /* 插件实现方法 */
-    public static function header($archive){
-        
+    /**
+	 * 
+	 * 
+	 * @access public
+	 * @return void
+	 */
+    public static function header(){
+        echo "
+        <style>
+    .memevideo
+    {
+        bottom: 0;
+        z-index: 52;
+        color: #666;
+        position: fixed;
+        user-select: none;
     }
-    public static function footer($archive){
+    .memevideo.left
+    {
+        left: 0;
+    }
+    .memevideo.right
+    {
+        right: 0;
+    }
+    .unshowctrl.video::-webkit-media-controls{
+        display: none;
+    }
+    .showctrl.video::-webkit-media-controls{
+        display: block;
+    }
+</style>
+        ";
+    }
+    /**
+	 * 
+	 * 
+	 * @access public
+	 * @return void
+	 */
+    public static function footer(){
+        function getHTMLvideo()
+        {
+             $auto = Typecho_Widget::widget('Widget_Options') -> Plugin('memevideo') -> autoplay;
+             $lop = Typecho_Widget::widget('Widget_Options') -> Plugin('memevideo') -> loop;
+             $mute = Typecho_Widget::widget('Widget_Options') -> Plugin('memevideo') -> muted;
+             $showctrl = Typecho_Widget::widget('Widget_Options') -> Plugin('memevideo') -> controls;
+             $link = Typecho_Widget::widget('Widget_Options') -> Plugin('memevideo') -> src;
+            $w = Typecho_Widget::widget('Widget_Options') -> Plugin('memevideo') -> custom_width;
+            $h = Typecho_Widget::widget('Widget_Options') -> Plugin('memevideo') -> custom_height;
+            return '<video id="mvideo" src=" '. ($link) .' " '. ($auto ? "autoplay='autoplay'" : "") .' '.($mute ? "muted='muted'" : "").' '. ($lop ? "loop='loop'" : "") .' width='.($w ? $w : 100).' height='.($h ? $h : 100).'  '. ($showctrl ? "controls='controls'" : "") .'>您的浏览器不支持 video 标签。</video>';
+        }
+        
+        function getVideoDiv()
+        {
+            $height = Typecho_Widget::widget('Widget_Options') -> Plugin('memevideo') -> custom_height;
+            $width = Typecho_Widget::widget('Widget_Options') -> Plugin('memevideo') -> custom_width;
+            $pos = Typecho_Widget::widget('Widget_Options') -> Plugin('memevideo') -> position;
+
+            return '<div class="memevideo '.($pos == "right" ? "right" : "left").' "> '.(getHTMLvideo()).' </div>';
+        }
+        echo (getVideoDiv());
         
     }
 }
--- README.md
+++ README.md
@@ -3,12 +3,12 @@
 - 在页面左下或右下放置一个指定宽高的视频
 - 用HTML5自带播放器
 - 自动+循环播放
-- 跳转到其他的站内页继续播放  
+- 跳转到其他的站内页继续播放  *(TODO)*
 
 ## 食用方法
 1. [下载最新版zip压缩包](https://github.com/qaqxhy/meme_video/archive/refs/heads/main.zip)
 2.  将压缩包上传至服务器```/usr/plugins```文件夹内
-3.  解压并重命名文件夹为```meme_video```
+3.  解压并重命名文件夹为```memevideo```
 4.  在Typecho插件管理页面启用插件  
 ## 更新日志
 ### 12.10.22 创建文件 版本 0.1
